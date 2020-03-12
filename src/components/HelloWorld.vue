09b3e410b2b4a7f3c56991ab9f980334ce7cd8fc
Yokioo <763431735@qq.com>
2020-03-12 14:55:58 +0800

    vue移动端适配插件lib-flexible

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="hello">
+    适配移动端
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.hello {
+  width: 200px;
+  height: 200px;
+  font-size: 36px;
+  margin: 0 auto;
+  box-sizing: border-box;
+  border: 1px solid green;
+}
+</style>
